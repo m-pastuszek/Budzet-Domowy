76924a8ceeac3e93e2f178e3858458ce20f17dd9
Marcin Pastuszek <m.pastuszek@moj-sklep.pl>
2021-03-28 12:08:30 +0200

    Dodaj pliki projektów.

---
+++ Budżet Domowy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Budżet_Domowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
