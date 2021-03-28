16983b3bbefc6bc9ebd108a0d33f0eb0fa8162af
Marcin Pastuszek <m.pastuszek@moj-sklep.pl>
2021-03-28 14:07:09 +0200

    Dodanie klas

---
+++ Budżet Domowy/Classes/Rola.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budżet_Domowy.Classes
+{
+    class Rola
+    {
+        private int m_iId;
+        private string m_sNazwa;
+        private string m_sOpis;
+
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string Nazwa
+        {
+            get;
+            set;
+        }
+
+        public string Opis
+        {
+            get;
+            set;
+        }
+    }
+}
